03b97e52803b02d35864bcae687b66b6366cc661
Roxxy28 <95098563+Roxxy28@users.noreply.github.com>
2022-10-18 17:40:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -100,9 +100,9 @@ function playRound() {
 }
 
 
-// function playRockPaperScissors() {
-//     while (playRound()) { }
-// }
+function playRockPaperScissors() {
+    while (playRound()) { }
+}
 
 // Call this function to play the game
 playRockPaperScissors()
